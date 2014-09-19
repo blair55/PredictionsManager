298c79747e6da010df892069dcd3e55460a9df13
Nick <blair.nicholas@googlemail.com>
2014-09-19 18:41:12 +0100

    more data

--- PredictionsManager.Domain/Data.fs
+++ PredictionsManager.Domain/Data.fs
@@ -2,20 +2,54 @@
 
 open FSharp.Data
 open PredictionsManager.Domain.Domain
+open System
 open System.IO
 
 module Data =
 
-    //let pCsv = new CsvProvider<"Y:/Code/PredictionsManager/Data/Predictions.csv">()
-
     [<Literal>]
-    let fileName = "Y:/Code/PredictionsManager/Data/Predictions.csv"
-    let header = "fixtureId,player,homeScore,awayScore"
+    let dir = "Y:/Code/PredictionsManager/Data/"
+    [<Literal>]
+    let fFilePath = dir + "fixtures.csv"
+    [<Literal>]
+    let rFilePath = dir + "results.csv"
+    [<Literal>]
+    let pFilePath = dir + "predictions.csv"
+    
+    let fHeader = "id,gameWeekNo,home,away"
+    let rHeader = "fixtureId,homeScore,awayScore"
+    let pHeader = "fixtureId,player,homeScore,awayScore"
+    let fToCsvString (f:Fixture) = sprintf "%s,%i,%s,%s" (getFxId f.id) (getGameWeekNo f.gameWeek.number) f.home f.away
+    let rToCsvString (r:Result) = sprintf "%s,%i,%i" (getFxId r.fixture.id) (fst r.score) (snd r.score)
+    let pToCsvString (p:Prediction) = sprintf "%s,%s,%i,%i" (getFxId p.fixture.id) (getPlayerName p.player) (fst p.score) (snd p.score)
+        
+    let initList l filePath header map =
+        let lines = l |> List.map(map)
+        let linesAndHead = header::lines |> List.toArray
+        File.WriteAllLines(filePath, linesAndHead)
 
-    let storePredictions (predictions:Prediction list) =
-        let predictionToCsvString (p:Prediction) =
-            sprintf "%s,%s,%i,%i" (getFxId p.fixture.id) (getPlayerName p.player) (fst p.score) (snd p.score)
-        let l = predictions |> List.map(predictionToCsvString) |> List.toArray
-        File.WriteAllLines(fileName, [|header|])
-        File.WriteAllLines(fileName, l)
+    let initFixtures (f:Fixture list) = initList f fFilePath fHeader fToCsvString
+    let initResults (r:Result list) = initList r rFilePath rHeader rToCsvString
+    let initPredictions (p:Prediction list) = initList p pFilePath pHeader pToCsvString
 
+    let initAll (f:Fixture list) (r:Result list) (p:Prediction list) =
+        initFixtures f
+        initResults r
+        initPredictions p
+
+    // type Stocks = CsvProvider<"../data/MSFT.csv">
+    // let msft = Stocks.Load("http://ichart.finance.yahoo.com/table.csv?s=MSFT")
+
+    type fCsv = CsvProvider<fFilePath>
+    type rCsv = CsvProvider<rFilePath>
+    type pCsv = CsvProvider<pFilePath>
+
+    let fData = fCsv.Load(fFilePath)
+    let rData = rCsv.Load(rFilePath)
+    let pData = pCsv.Load(pFilePath)
+    
+    let getGuid s = Guid.Parse s
+
+    let getFixtures() =
+        fData.Rows |> Seq.map(fun x -> { Fixture.id=(getGuid x.id); gameWeek })
+        
